96b6f07b44b07dbe5d1ac738b8d9b4b55fcd80be
Maxim Turbal <tor968@yahoo.com>
2020-07-22 22:11:40 +0300

    Update task_1.js

--- js/task_1.js
+++ js/task_1.js
@@ -1,11 +1,9 @@
-const name = '«Генератор защитного поля»'; // Write code in this line
-const price = 1000; // Write code in this line
-firstMessage = ``; // Write code in this line
+const name = '«Генератор защитного поля»';
+let price = 1000;
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
 price = 2000;
-secondMessage = ``; // Write code in this line
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
 
-// console.log(firstMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+console.log(firstMessage);
 
-// console.log(secondMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
+console.log(secondMessage);
